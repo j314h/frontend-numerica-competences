21531c12fd5b35ef7d1a620ab53ce003a4b1dd68
j314h <johnhaimez314@gmail.com>
2021-03-01 13:14:11 +0100

    finish headband

--- src/components-globals/Titles/H1Component.vue
+++ src/components-globals/Titles/H1Component.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="h1_title_component">{{ text }}</h1>
+    <h1 :class="styleCustom">{{ text }}</h1>
   </div>
 </template>
 
@@ -9,6 +9,7 @@ export default {
   name: "H1Component",
   props: {
     text: { type: String, default: "Title h1" },
+    styleCustom: { type: String, default: "h1_title_component" },
   },
 };
 </script>
---
+++ src/components-globals/Titles/H3Component.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3 :class="styleCustom">{{ text }}</h3>
+</template>
+
+<script>
+export default {
+  name: "H3Component",
+  props: {
+    text: { type: String, default: "Title h3" },
+    styleCustom: { type: String, default: "h3_title_component" },
+  },
+};
+</script>
+
+<style></style>
